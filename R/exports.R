cbf55f10d997541af87953944765d46ac643d28c
Miguel <miguel.serrano.martin@ine.es>
2025-02-18 20:04:25 +0100

    recoded almost everything

--- R/exports.R
+++ R/exports.R
@@ -1,11 +1,36 @@
+#' Aggregate and repeat
+#'
+#' Helper function to repeat the aggregate annual value of a series on
+#' each period.
+#' @param x (ts) Any time series
+#' @param fun (function) Aggregation function, mean by default
+#' @details
+#' Applies \code{aggregate.ts} to the series to get the annual values
+#' and then repeats those values for every subyear period.
+#' @returns description
+#' @examples
+#' rep_year_value(gdp_es_index) |> plot()
+#' @export
+aggr_and_rep <- function(x, fun = mean) {
+  if (methods::is(x, "mts")) {
+    y <- apply_to_columns(x, aggr_and_rep)
+  } else {
+    f <- frequency(x)
+    ratio <- floor(length(x)/f)
+    aux <- aggregate.ts(x, FUN = fun) |> rep(times = rep(f, ratio))
+    y <- ts(aux, start = start(x), frequency = f)
+  }
+  return(y)
+}
+
 #' Apply method to multivariate
 #'
-#' This function applies a function for univariate series ("ts") to a multivariate
-#' ("mts").
+#' This function applies a function for univariate series ("ts") to a
+#' multivariate series ("mts").
 #' @param x A multivariate time series of class "mts".
 #' @param f A function that takes an univariate series as input.
 #' @param ... Arguments for \code{f}.
-apply_multivariate <- function(x, f, ...){
+apply_to_columns <- function(x, f, ...){
   n <- ncol(x)
   y <- lapply(1:n, \(i) {
     z <- f(x[,i], ...)
@@ -21,24 +46,16 @@ apply_multivariate <- function(x, f, ...){
 #'
 #' Computes the pyp index series from a chain-linked series.
 #'
-#' @param x A chain-linked series (with annual overlap) of class "ts".
+#' @param x (ts) Chain-linked series with annual overlap.
+#' @param x_a (ts) Annual chain-linked series.
 #' @returns The pyp series of class "ts".
 #' @examples
-#' set.seed(23)
-#' x <- sample(95:105, 24, TRUE) |>
-#'   ts(start = 2005, frequency = 4)
-#' y <- x |> chain_from_pyp(2006)
-#' (x - pyp_from_chain(y)) |> round(5)
-#' set.seed(23)
-#' x <- sample(95:105, 24, TRUE) |>
-#'   matrix(ncol = 2) |>
-#'   ts(start = 2005, frequency = 4)
-#' y <- x |> chain_from_pyp(2006)
-#' (x - pyp_from_chain(y)) |> round(5)
+#' get_pyp(gdp_es_volume)
+#'
 #' @export
-pyp_from_chain <- function(x) {
+get_pyp <- function(x, x_a = NULL) {
   if (methods::is(x, "mts")) {
-    x_pyp <- apply_multivariate(x, pyp_from_chain)
+    x_pyp <- apply_to_columns(x, get_pyp)
   } else {
     if (!("ts" %in% class(x))) {
       stop("<x> is not 'ts'.")
@@ -46,12 +63,15 @@ pyp_from_chain <- function(x) {
     if ((frequency(x) != 1) & (start(x)[2] != 1)) {
       stop("<x> must start at the beginning of the year.")
     }
-    x_a <- aggregate.ts(x, FUN = mean)
-    x_a_lagged <- lag(x_a, k = -1)
-    x_a_aux <- x_a_lagged[rep(1:length(x_a),
-                              times = rep(frequency(x), length(x_a)))] |>
-      ts(start = start(x_a_lagged), frequency = frequency(x))
-    x_pyp <- x/x_a_aux*100
+    if (is.null(x_a)) {
+      aux <- aux <- aggr_and_rep(x)
+    } else {
+      f <- frequency(x)
+      ratio <- floor(length(x)/f)
+      aux <- rep(x_a, times = rep(f, ratio)) |>
+        ts(aux, start = start(x), frequency = f)
+    }
+    x_pyp <- x/aux*100
   }
   return(x_pyp)
 }
@@ -68,29 +88,17 @@ pyp_from_chain <- function(x) {
 #' @importFrom stats frequency
 #' @importFrom stats window
 #' @examples
-#' set.seed(23)
-#' x <- sample(95:105, 12, TRUE) |>
-#'   ts(start = 2005, frequency = 4) |>
-#'   chain_from_pyp(2006)
-#' aggregate(x, FUN = mean)
-#' y <- change_ref_year(x, 2007)
-#' aggregate(y, FUN = mean)
-#' set.seed(23)
-#' x <- sample(95:105, 24, TRUE) |>
-#'   matrix(ncol = 2) |>
-#'   ts(start = 2005, frequency = 4) |>
-#'   chain_from_pyp(2006)
-#' aggregate(x, FUN = mean)
-#' y <- change_ref_year(x, 2007)
-#' aggregate(y, FUN = mean)
+#' change_ref_year(gdp_es_volume, 2015)
+#' plot(gdp_es_volume)
+#' lines(change_ref_year(gdp_es_volume, 2015))
+#'
 #' @export
 change_ref_year <- function(x, new_ref) {
   if (methods::is(x, "mts")) {
-    y <- apply_multivariate(x, change_ref_year_uni, new_ref)
+    y <- apply_to_columns(x, change_ref_year_uni, new_ref)
   } else {
     denom <- window(x, start = c(new_ref,1),
-                    end = c(new_ref, frequency(x))) |>
-      mean()
+                    end = c(new_ref, frequency(x))) |> mean()
     y <- x / denom * 100
   }
   return(y)
@@ -111,11 +119,14 @@ change_ref_year <- function(x, new_ref) {
 #' Suppose the x series runs from (y0, p0 = 0) to (y1, p1), where pi is a subyear
 #' period. Then the chain-linked series at (y2, p2) is given by the cumulative
 #' product of the annual series from y0 to y2-1 times x at (y2, p2).
+#' @examples
+#' gdp_es_pyp <- get_pyp(gdp_es_volume)
+#' get_chain_linked(gdp_es_pyp)
 #'
 #' @export
-chain_from_pyp <- function(x, ref_year, x_a = NULL) {
+get_chain_linked <- function(x, ref_year, x_a = NULL) {
   if (methods::is(x, "mts")) {
-    y <- apply_multivariate(x, chain_from_pyp_uni, ref_year)
+    y <- apply_to_columns(x, chain_from_pyp_uni, ref_year)
   } else {
     if (frequency(x) == 1) {
       x_chain <- cumprod(c(100,x/100)) |> ts(start = start(x)[1] - 1)
@@ -125,7 +136,7 @@ chain_from_pyp <- function(x, ref_year, x_a = NULL) {
       if (is.null(x_a)) {
         x_a <- aggregate.ts(x, FUN = mean)
       }
-      x_chain_a <- chain_from_pyp(x_a, ref_year)
+      x_chain_a <- get_chain_linked(x_a, ref_year)
       s <- frequency(x)
       x_chain_a_aux <- x_chain_a |> rep(times = rep(s,length(x_chain_a))) |>
         ts(start = start(x_chain_a)[1] + 1, frequency = s)
@@ -137,82 +148,59 @@ chain_from_pyp <- function(x, ref_year, x_a = NULL) {
   return(y)
 }
 
-#' Get volume index from current and pyp prices
+#' Get quantity index
 #'
-#' Returns the series of pyp volume indices given current prices
-#' and pyp prices.
+#' Returns the series of quantity indices in previous year prices from
+#' a current prices and
 #' @param current Current prices series of class "ts".
 #' @param constant Constant prices (pyp) series of class "ts".
 #' @returns List of time series ("ts"): the quarterly IPs and IQs (pyp).
+#' @examples
+#' gdp_es_pyp <- get_pyp(gdp_es_volume)
+#' gdp_es_constant <- gdp_es_current / gdp_es_constant * 100
+#' get_q_index(gdp_es_current, gdp_es_constant)
+#'
 #' @export
-iq_pyp_from_money <- function(current, constant) {
-  if (methods::is(current, "mts")) {
+get_q_index <- function(current, constant) {
+  if (methods::is(current, "mts") & methods::is(constant, "mts")) {
     if(ncol(current) != ncol(constant)) {
       stop("current and constant don't have the same number of columns!")
     }
     n <- ncol(current)
     y <- lapply(1:n, \(i) {
-      z <- iq_pyp_from_money(current[, i], constant[, i])
+      z <- get_q_index(current[, i], constant[, i])
       w <- ts(z, start = start(z), frequency = frequency(z))
       return(w)
     })
     result <- do.call(cbind, y)
     colnames(result) <- colnames(current)
-  } else {
-    current_aux <- current |>
-      aggregate.ts(FUN = mean) |>
-      rep(times = rep(frequency(current),
-                      times = floor(length(current)/frequency(current)))) |>
-      ts(start = start(current)[1] + 1, frequency = frequency(current))
-    result <- constant / current_aux * 100
+  } else if (!methods::is(current, "mts") & !methods::is(constant, "mts")) {
+    aux <- aggr_and_rep(current) |> stats::lag(-frequency(current))
+    result <- constant / aux * 100
   }
   return(result)
 }
 
-#' Get chain-linked volume measure
+#' Get value index
 #'
-#' Returns chain-linked volume given chain-linked volume indices and current prices.
-#' @param iq_chain Chain-linked volume indices.
-#' @param current Current prices.
-#' @param ref_year Reference year for the chain-linked indices.
-#' @returns The chain-linked volume measures.
-#' @export
-chain_vol_from_iq <- function(iq_chain, current, ref_year){
-  s <- frequency(iq_chain)
-  if (methods::is(current, "mts")) {
-    vol_chain <- sapply(1:ncol(iq_chain), \(i) {
-      c(sum(window(current[,i], start = ref_year, end = c(ref_year, s))))*iq_chain[,i]/400
-    }) |> ts(start = start(iq_chain), frequency = s)
-    colnames(vol_chain) <- colnames(current)
-  } else {
-    vol_chain <- c(sum(window(current, start = ref_year, end = c(ref_year, s))))*iq_chain/400
-  }
-  return(vol_chain)
-}
-
-#' Get value index from current prices
+#' Returns the (not chain-linked) series of value indices from a series
+#' of current prices.
+#' @param current (ts) Series of current prices series.
+#' @returns (ts) Series of value indices.
+#' @details
+#' The value of the resulting series x at (y,s), where y is the year and s
+#' is the subyear period, is current(y,s)/current(y)
 #'
-#' Returns the series of pyp value indices given current prices.
-#' @param current Current prices series of class "ts".
-#' @returns the series of IVs (pyp).
+#' @examples
+#' get_v_index(gdp_es_current)
+#'
 #' @export
-iv_pyp_from_money <- function(current) {
+get_v_index <- function(current) {
   if (methods::is(current, "mts")) {
-    n <- ncol(current)
-    y <- lapply(1:n, \(i) {
-      z <- iv_pyp_from_money(current[, i])
-      w <- ts(z, start = start(z), frequency = frequency(z))
-      return(w)
-    })
-    result <- do.call(cbind, y)
-    colnames(result) <- colnames(current)
+    y <- apply_multivariate(current, get_v_index)
   } else {
-    aux <- current |>
-      aggregate.ts(FUN = mean) |>
-      rep(times = rep(frequency(current),
-                      times = floor(length(current)/frequency(current)))) |>
-      ts(start = start(current)[1] + 1, frequency = frequency(current))
-    result <- current / aux * 100
+    aux <- aggr_and_rep(current) |> stats::lag(-frequency(current))
+    y <- current / aux * 100
   }
-  return(result)
+  return(y)
 }
