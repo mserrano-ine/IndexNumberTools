8a5dc80198150104de5a1ce80ac926577cf200ff
Miguel <miguel.serrano.martin@ine.es>
2025-02-19 21:31:46 +0100

    check passing falta doc data

---
+++ R/IndexNumberTools-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats aggregate.ts
+#' @importFrom stats frequency
+#' @importFrom stats start
+#' @importFrom stats ts
+#' @importFrom stats window
+## usethis namespace: end
+NULL
--- R/functions.R
+++ R/functions.R
@@ -9,7 +9,7 @@
 #' and then repeats those values for every subyear period.
 #' @returns description
 #' @examples
-#' rep_year_value(gdp_es_index) |> plot()
+#' aggr_and_rep(gdp_volume) |> plot()
 #' @export
 aggr_and_rep <- function(x, fun = mean) {
   if (methods::is(x, "mts")) {
@@ -27,8 +27,8 @@ aggr_and_rep <- function(x, fun = mean) {
 #'
 #' This function applies a function for univariate series ("ts") to a
 #' multivariate series ("mts").
-#' @param x A multivariate time series of class "mts".
-#' @param f A function that takes an univariate series as input.
+#' @param x (mts) A multivariate time series.
+#' @param f (function) A function that takes an univariate series as input.
 #' @param ... Arguments for \code{f}.
 apply_to_columns <- function(x, f, ...){
   n <- ncol(x)
@@ -47,29 +47,28 @@ apply_to_columns <- function(x, f, ...){
 #' Computes the pyp index series from a chain-linked series.
 #'
 #' @param x (ts) Chain-linked series with annual overlap.
-#' @param x_a (ts) Annual chain-linked series.
-#' @returns The pyp series of class "ts".
+#' @param x_a (ts) Annual chain-linked series. If not given, it's computed by
+#' taking the average of each year.
+#' @returns The pyp series.
+#' @details
+#' The time series should start at (y,1) where y is any year.
+#'
 #' @examples
-#' get_pyp(gdp_es_volume)
+#' get_pyp(gdp_volume)
 #'
 #' @export
 get_pyp <- function(x, x_a = NULL) {
+  f <- stats::frequency(x)
+  ratio <- floor(length(x)/f)
   if (methods::is(x, "mts")) {
     x_pyp <- apply_to_columns(x, get_pyp)
   } else {
-    if (!("ts" %in% class(x))) {
-      stop("<x> is not 'ts'.")
-    }
-    if ((frequency(x) != 1) & (start(x)[2] != 1)) {
-      stop("<x> must start at the beginning of the year.")
-    }
     if (is.null(x_a)) {
-      aux <- aux <- aggr_and_rep(x)
+      aux <- aggr_and_rep(x) |> stats::lag(-f)
     } else {
-      f <- frequency(x)
-      ratio <- floor(length(x)/f)
       aux <- rep(x_a, times = rep(f, ratio)) |>
-        ts(aux, start = start(x), frequency = f)
+        stats::ts(start = stats::start(x), frequency = f) |>
+        stats::lag(-f)
     }
     x_pyp <- x/aux*100
   }
@@ -80,25 +79,21 @@ get_pyp <- function(x, x_a = NULL) {
 #'
 #' Changes the reference year of a chain-linked series (with annual
 #' overlap).
-#' @param x A chain-linked series (with annual overlap) of class "ts".
-#' @param new_ref New reference year. Must be such that \code{start(x) <=
-#' new_ref <= end(x)}.
-#' @returns The re-referenced index series of class "ts".
-#' @importFrom methods is
-#' @importFrom stats frequency
-#' @importFrom stats window
+#' @param x (ts) A chain-linked series with annual overlap.
+#' @param new_ref_year (num) New reference year.
+#' @returns The re-referenced index series.
 #' @examples
-#' change_ref_year(gdp_es_volume, 2015)
-#' plot(gdp_es_volume)
-#' lines(change_ref_year(gdp_es_volume, 2015))
+#' change_ref_year(gdp_volume, 2015)
+#' plot(gdp_volume)
+#' lines(change_ref_year(gdp_volume, 2015))
 #'
 #' @export
-change_ref_year <- function(x, new_ref) {
+change_ref_year <- function(x, new_ref_year) {
   if (methods::is(x, "mts")) {
-    y <- apply_to_columns(x, change_ref_year_uni, new_ref)
+    y <- apply_to_columns(x, change_ref_year, new_ref_year)
   } else {
-    denom <- window(x, start = c(new_ref,1),
-                    end = c(new_ref, frequency(x))) |> mean()
+    denom <- stats::window(x, start = c(new_ref_year,1),
+                    end = c(new_ref_year, stats::frequency(x))) |> mean()
     y <- x / denom * 100
   }
   return(y)
@@ -108,25 +103,24 @@ change_ref_year <- function(x, new_ref) {
 #'
 #' Computes chain-linked index series from a pyp series.
 #'
-#' @param x A pyp series of class "ts".
-#' @param ref_year Reference year ("num") for the chain-linked series.
-#' @param x_a Annual pyp series. If not given, it is assumed that it's computed
-#' like \code{x_a = aggregate.ts(x, FUN = mean)}.
-#' @param normalize Make reference year = 100.
-#' @returns The chain-linked series of class "ts".
+#' @param x (ts) A pyp series.
+#' @param ref_year (num) Reference year for the chain-linked series.
+#' @param x_a (ts) Annual pyp series. If not given, it's computed by taking the
+#' average of each year.
+#' @returns The chain-linked series.
 #' @details
 #' The chain-linked series x_chain is computed with the annual overlap method.
 #' Suppose the x series runs from (y0, p0 = 0) to (y1, p1), where pi is a subyear
 #' period. Then the chain-linked series at (y2, p2) is given by the cumulative
 #' product of the annual series from y0 to y2-1 times x at (y2, p2).
 #' @examples
-#' gdp_es_pyp <- get_pyp(gdp_es_volume)
-#' get_chain_linked(gdp_es_pyp)
+#' gdp_pyp <- get_pyp(gdp_volume)
+#' get_chain_linked(gdp_pyp, 2015)
 #'
 #' @export
 get_chain_linked <- function(x, ref_year, x_a = NULL) {
   if (methods::is(x, "mts")) {
-    y <- apply_to_columns(x, chain_from_pyp_uni, ref_year)
+    y <- apply_to_columns(x, get_chain_linked, ref_year)
   } else {
     if (frequency(x) == 1) {
       x_chain <- cumprod(c(100,x/100)) |> ts(start = start(x)[1] - 1)
@@ -152,20 +146,16 @@ get_chain_linked <- function(x, ref_year, x_a = NULL) {
 #'
 #' Returns the series of quantity indices in previous year prices from
 #' a current prices and
-#' @param current Current prices series of class "ts".
-#' @param constant Constant prices (pyp) series of class "ts".
-#' @returns List of time series ("ts"): the quarterly IPs and IQs (pyp).
+#' @param current (ts) Values at current prices.
+#' @param constant (ts) Values at previous year prices.
+#' @returns Series of quantity indices for previous year prices.
 #' @examples
-#' gdp_es_pyp <- get_pyp(gdp_es_volume)
-#' gdp_es_constant <- gdp_es_current / gdp_es_constant * 100
-#' get_q_index(gdp_es_current, gdp_es_constant)
-#'
+#' gdp_pyp <- get_pyp(gdp_volume)
+#' gdp_constant <- gdp_current / gdp_pyp * 100
+#' get_q_index(gdp_current, gdp_constant)
 #' @export
 get_q_index <- function(current, constant) {
   if (methods::is(current, "mts") & methods::is(constant, "mts")) {
-    if(ncol(current) != ncol(constant)) {
-      stop("current and constant don't have the same number of columns!")
-    }
     n <- ncol(current)
     y <- lapply(1:n, \(i) {
       z <- get_q_index(current[, i], constant[, i])
@@ -192,12 +182,12 @@ get_q_index <- function(current, constant) {
 #' is the subyear period, is current(y,s)/current(y)
 #'
 #' @examples
-#' get_v_index(gdp_es_current)
+#' get_v_index(gdp_current)
 #'
 #' @export
 get_v_index <- function(current) {
   if (methods::is(current, "mts")) {
-    y <- apply_multivariate(current, get_v_index)
+    y <- apply_to_columns(current, get_v_index)
   } else {
     aux <- aggr_and_rep(current) |> stats::lag(-frequency(current))
     y <- current / aux * 100
